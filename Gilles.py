9c5fa27fe16958c3b4619bc2ef4cb1efc91580b6
Gilles Belmans <gilles.belmans@hotmail.com>
2021-03-01 21:51:28 +0100

    nieuw

--- Gilles.py
+++ Gilles.py
@@ -1,2 +1,8 @@
 print("welkom")
 print("Gilles ge kunt het niet")
+
+letters = int(input())
+for _ in range(letters):
+    positie, woord = input().rstrip().split()
+    print(woord[int(positie) - 1])
+
